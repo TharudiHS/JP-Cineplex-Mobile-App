33289ff54c37c34800c6e0e63dbd10a356eba9f0
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-20 12:45:17 +0530

    add text_styles.dart file

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'app_colours.dart';
+
+class TextStyles {
+  static const TextStyle size14WeightBoldConthraxSemiBold = TextStyle(
+    fontFamily: 'Conthrax',
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    color: AppColours.white,
+  );
+  static const TextStyle size14PromptLight = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 14,
+    color: AppColours.lightGrey,
+  );
+  static const TextStyle size20WeightBoldConthraxSemiBold = TextStyle(
+    fontFamily: 'Conthrax',
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColours.white,
+  );
+  static const TextStyle size14ConthraxSemiBold = TextStyle(
+    fontFamily: 'Conthrax',
+    fontSize: 14,
+    color: AppColours.black,
+  );
+}
