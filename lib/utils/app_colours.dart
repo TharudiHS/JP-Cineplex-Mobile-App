ef5566632c3bba8000995d1a8132efec4a35daef
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-21 17:24:10 +0530

    added new colours

--- lib/utils/app_colours.dart
+++ lib/utils/app_colours.dart
@@ -13,4 +13,18 @@ class AppColours {
   static const grey48 = Color(0xFF484848);
   static const grey11 = Color(0xFF111111);
   static const grey5E = Color(0xFF5E5E5E);
+  static const grey20 = Color(0xFF202020);
+  static const grey17 = Color(0xFF171717);
+  static const blackTransparent20 = Color(0x33000000);
+  static const grey58 = Color(0xFF58595B);
+  static const goldDark = Color(0xFFCEA347);
+  static const purple = Color(0xFF5C01FF);
+  static const orange = Color(0xFFFF8345);
+  static const green = Color(0xFF1AA77C);
+  static const blue = Color(0xFF0071CD);
+  static const grey75 = Color(0xFF757575);
+  static const grey2E = Color(0xFF2E2E2E);
+  static const grey55 = Color(0xFF555555);
+  static const grey30 = Color(0xFF303030);
+  static const goldBrown = Color(0xFFB4A159);
 }
