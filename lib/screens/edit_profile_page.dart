955fd4e9e5f8172065d0efd0228e3990bfdf7bc2
TharudiHS <tharudihimeshaaa@gmail.com>
2025-08-30 09:31:44 +0530

    updated code

--- lib/screens/edit_profile_page.dart
+++ lib/screens/edit_profile_page.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:jp_cinema_app/components/app_bar3.dart';
 import 'package:jp_cinema_app/components/main_button.dart';
 import 'package:jp_cinema_app/components/text_form_field.dart';
-
+import 'package:jp_cinema_app/screens/setting_page.dart';
 import '../utils/app_colours.dart';
 import '../utils/text_styles.dart';
 
-class EditProfilePage extends StatelessWidget {
-  EditProfilePage({super.key});
+class EditProfilePage extends StatefulWidget {
+  const EditProfilePage({super.key});
 
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
   // Controllers
   final TextEditingController nameController = TextEditingController(
     text: "Kanchani Rathnayake",
@@ -25,24 +31,13 @@ class EditProfilePage extends StatelessWidget {
   final TextEditingController confirmPasswordController =
       TextEditingController();
 
+  String gender = "Female";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColours.black,
-      appBar: AppBar(
-        backgroundColor: AppColours.black,
-        elevation: 0,
-        title: Text(
-          "Edit Profile",
-          style: TextStyles.size16WeightBoldConthraxSemiBold,
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
+      appBar: buildAppBar3(context, "Edit Profile"),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -51,13 +46,13 @@ class EditProfilePage extends StatelessWidget {
             // Profile Info label
             Text(
               "Profile information:",
-              style: TextStyles.size13WeightBoldConthraxSemiBoldgreyC2,
+              style: TextStyles.size14PromptLightwhite,
             ),
             const SizedBox(height: 10),
 
             // Name
             CustomTextFormField(controller: nameController, hint: "Full Name"),
-            const SizedBox(height: 12),
+            const SizedBox(height: 14),
 
             // Email
             CustomTextFormField(
@@ -65,7 +60,7 @@ class EditProfilePage extends StatelessWidget {
               hint: "Email",
               keyboardType: TextInputType.emailAddress,
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 14),
 
             // Phone
             CustomTextFormField(
@@ -73,7 +68,7 @@ class EditProfilePage extends StatelessWidget {
               hint: "Phone Number",
               keyboardType: TextInputType.phone,
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 14),
 
             // Gender Dropdown
             Container(
@@ -83,34 +78,38 @@ class EditProfilePage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(4),
               ),
               child: DropdownButton<String>(
-                value: "Female",
+                value: gender,
                 dropdownColor: AppColours.darkGrey,
                 isExpanded: true,
                 underline: const SizedBox(),
-                style: const TextStyle(color: Colors.white),
+                style: TextStyles.size13WeightBoldConthraxSemiBoldgreyC2,
                 items: const [
                   DropdownMenuItem(value: "Male", child: Text("Male")),
                   DropdownMenuItem(value: "Female", child: Text("Female")),
                   DropdownMenuItem(value: "Other", child: Text("Other")),
                 ],
-                onChanged: (value) {},
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      gender = value;
+                    });
+                  }
+                },
               ),
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 14),
 
             // DOB
             CustomTextFormField(
               controller: dobController,
               hint: "Date of Birth",
+              textStyle: TextStyles.size13WeightBoldConthraxSemiBoldgreyC2,
               suffixIcon: const Icon(Icons.calendar_today, color: Colors.white),
             ),
             const SizedBox(height: 20),
 
             // Password Section
-            Text(
-              "New Password:",
-              style: TextStyles.size13WeightBoldConthraxSemiBoldgreyC2,
-            ),
+            Text("New Password:", style: TextStyles.size14PromptLightwhite),
             const SizedBox(height: 10),
             CustomTextFormField(
               controller: passwordController,
@@ -119,10 +118,7 @@ class EditProfilePage extends StatelessWidget {
             ),
             const SizedBox(height: 12),
 
-            Text(
-              "Confirm password:",
-              style: TextStyles.size13WeightBoldConthraxSemiBoldgreyC2,
-            ),
+            Text("Confirm password:", style: TextStyles.size14PromptLightwhite),
             const SizedBox(height: 10),
             CustomTextFormField(
               controller: confirmPasswordController,
@@ -135,7 +131,21 @@ class EditProfilePage extends StatelessWidget {
             MainButton(
               label: "SAVE",
               onPressed: () {
-                // Save action
+                if (passwordController.text.isNotEmpty &&
+                    passwordController.text != confirmPasswordController.text) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Passwords do not match"),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                  return;
+                }
+
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const SettingsPage()),
+                );
               },
               backgroundColor: AppColours.gold,
               textStyle: TextStyles.size14WeightBoldConthraxSemiBoldblack,
